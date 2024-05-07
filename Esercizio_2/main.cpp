27b7cc58d5dc9c98ac45545cdec53c7dfcd0f0e7
unknown <s295373@studenti.polito.it>
2024-05-07 18:42:01 +0200

    My second commit

--- Esercizio_2/main.cpp
+++ Esercizio_2/main.cpp
@@ -46,14 +46,14 @@ int main(int argc, char ** argv)
         std::chrono::steady_clock::time_point t_b_Merge=std::chrono::steady_clock::now();
         MergeSort(v1);
         std::chrono::steady_clock::time_point t_e_Merge=std::chrono::steady_clock::now();
-        double durationMerge = std::chrono::duration_cast<std::chrono::microseconds>(t_e_Merge-t_b_Merge).count(); //misura una differenza di tempo
+        double durationMerge = std::chrono::duration_cast<std::chrono::microseconds>(t_e_Merge-t_b_Merge).count();
         cout<< "MergeSort algorithm took " << durationMerge<< " microseconds"<<endl;
         sumMerge += durationMerge;
 
         std::chrono::steady_clock::time_point t_b_Bubble=std::chrono::steady_clock::now();
         BubbleSort(v2);
         std::chrono::steady_clock::time_point t_e_Bubble=std::chrono::steady_clock::now();
-        double durationBubble = std::chrono::duration_cast<std::chrono::microseconds>(t_e_Bubble-t_b_Bubble).count(); //misura una differenza di tempo
+        double durationBubble = std::chrono::duration_cast<std::chrono::microseconds>(t_e_Bubble-t_b_Bubble).count();
         cout<< "BubbleSort algorithm took " << durationBubble<< " microseconds"<<endl;
         sumBubble += durationBubble;
         cout << endl;
